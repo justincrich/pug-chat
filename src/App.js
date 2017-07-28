176cf6b79be22ec05534278ca20bee8f34f2b56b
Justin Rich <justinrich2008@gmail.com>
2017-07-27 17:35:45 -0700

    convo list view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import home from './pages/home/home';
 
-class App extends Component {
+class Routes extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={home}/>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './normalize.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Apollo client handles both state and data for this app
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({uri:'https://api.graph.cool/simple/v1/cj5k4zlmleo0p012250pwriwc'}),
+  dataIdFromObject: o => o.id
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+), document.getElementById('root'));
 registerServiceWorker();
